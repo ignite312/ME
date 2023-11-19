9c8b26a30de6d2544dade619012bd24323c60b21
ignite312 <contact.emonkhan@gmail.com>
2023-11-20 00:53:10 +0600

    Changed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import spectre from './spectre.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={spectre} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Will add some CP stuff Later
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://codeforces.com/profile/Tspectre"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Find me on CF...
         </a>
       </header>
     </div>
