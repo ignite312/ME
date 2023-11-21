dd4582e8b4c73909f1e4addf0d7dd8c3fcc2aae8
ignite312 <contact.emonkhan@gmail.com>
2023-11-22 03:42:05 +0600

    Added Stuff

--- src/GitHubInfo.js
+++ src/GitHubInfo.js
@@ -1,3 +1,4 @@
+/*
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import './Styles.css';
@@ -9,7 +10,7 @@ const GitHubInfo = () => {
   const fetchGitHubData = async () => {
     const userApiUrl = 'https://api.github.com/users/ignite312';
     const graphqlApiUrl = 'https://api.github.com/graphql';
-    const token = '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'; // Replace with your GitHub Personal Access Token
+    const token = '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';
 
     try {
       const userResponse = await fetch(userApiUrl);
@@ -86,6 +87,68 @@ const GitHubInfo = () => {
             </div>
           <p>Public Repositories: {githubData.public_repos}</p>
           <div style={{ display: 'flex', gap: '10px' }}>
+            <p>Followers: {githubData.followers}</p>
+            <p>Following: {githubData.following}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default GitHubInfo;
+*/
+
+
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import './Styles.css';
+import githubLogo from './github.png';
+
+const GitHubInfo = () => {
+  const [githubData, setGithubData] = useState(null);
+
+  const fetchGitHubData = async () => {
+    const userApiUrl = 'https://api.github.com/users/ignite312';
+
+    try {
+      const userResponse = await fetch(userApiUrl);
+
+      if (!userResponse.ok) {
+        throw new Error(`GitHub API request failed: ${userResponse.statusText}`);
+      }
+
+      const userData = await userResponse.json();
+
+      setGithubData({
+        login: userData.login,
+        public_repos: userData.public_repos,
+        followers: userData.followers,
+        following: userData.following,
+      });
+    } catch (error) {
+      console.error('Error fetching GitHub data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchGitHubData();
+  }, []);
+
+  return (
+    <div className="card">
+      <h3>GitHub</h3>
+      <img src={githubLogo} alt="GitHub Logo" className="logo" />
+      {githubData && (
+        <div className="card-content">
+          <h2>Handle: {githubData.login}</h2>
+          <div style={{ color: 'green' }}>
+            <span style={{ color: 'black' }}>Contribution Count: </span>
+            <strong>{'++'}</strong>
+            {/* <strong>{'+' + githubData.contributions}</strong> */}
+          </div>
+          <p>Public Repositories: {githubData.public_repos}</p>
+          <div style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
             <p>Followers: {githubData.followers}</p>
             <p>Following: {githubData.following}</p>
           </div>
