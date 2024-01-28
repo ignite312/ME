c731f3e273f276e3774a7c86a09cf56dabe4e666
ignite312 <contact.emonkhan@gmail.com>
2024-01-29 00:20:25 +0600

    Bug Fixed

--- src/Header.js
+++ src/Header.js
@@ -9,13 +9,13 @@ const Header = () => {
       <img src={spectre} alt="Spectre Logo" className="logo_h" />
         <div className="text-container">
           <h1 className="app-title">Hey</h1>
-          <p className="app-description">It's Emon, currently pursuing my undergraduate degree in the Department of Computer Science and Engineering at the University of Dhaka. I will add all kinds of academic and programming-related stuff here. For any kind of information related to academic or programming, feel free to reach out here<a className="header-link" href="mailto:schrodingersrats@gmail.com"> schrodingersrats@gmail.com</a></p>
+          <p className="app-description">It's Emon, currently pursuing my undergraduate degree in the Department of Computer Science and Engineering at the University of Dhaka. I will add all kinds of academic and programming-related stuff here. For any kind of information related to academic or programming, feel free to reach out here<a className="header-link2" href="mailto:schrodingersrats@gmail.com"> schrodingersrats@gmail.com</a></p>
         </div>
       </div>
       <div className="links-container">
+        <a href="#competitive-programming" className="header-link">CP Stuff</a>
         <a href="#about" className="header-link">About</a>
-        <a href="#contact" className="header-link">Contact</a>
-        <a href="#competitive-programming" className="header-link">Competitive Programming</a>
+        <a href="#contact" className="header-link">Contact Me </a>
       </div>
     </header>
   );
